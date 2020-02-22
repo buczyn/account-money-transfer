04ee84ff509bd6c7610af6b8f00efb9673b5e220
Jacek Bukowski <buczyn@gmail.com>
2020-02-22 16:43:19 +0100

    Adding logging.
    Using Logback as logging framework and SLF4J as logging API in the project.

--- src/main/java/org/example/amt/Application.java
+++ src/main/java/org/example/amt/Application.java
@@ -6,48 +6,60 @@ import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
+import org.glassfish.jersey.jaxb.internal.XmlJaxbElementProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 
 public class Application {
 
+    private final Logger log = LoggerFactory.getLogger(Application.class);
+
     private Undertow server;
 
     public static void main(String[] args) {
-        try {
             new Application().start();
-        } catch (ServletException e) {
-            // simple exception handler for now
-            throw new RuntimeException("Cannot start", e);
+    }
+
+    public void start()  {
+        if (server == null) {
+            DeploymentInfo servletBuilder = Servlets.deployment() //
+                    .setClassLoader(getClass().getClassLoader()) //
+                    .setContextPath("/") //
+                    .setDeploymentName("bank.war")//
+                    .addServlets(//
+                            Servlets.servlet("BankServlet", org.glassfish.jersey.servlet.ServletContainer.class)//
+                                    .addInitParam("javax.ws.rs.Application", "org.example.amt.rest.BankApplication")//
+                                    .addMapping("/*"));
+
+            DeploymentManager manager = Servlets.defaultContainer().addDeployment(servletBuilder);
+            manager.deploy();
+
+            try {
+                PathHandler path = Handlers.path(manager.start());
+
+                server = Undertow.builder()
+                        .addHttpListener(8080, "localhost")
+                        .setIoThreads(2)
+                        .setWorkerThreads(10)
+                        .setHandler(path)
+                        .build();
+                log.info("Starting server.");
+                server.start();
+            } catch (Exception e) {
+                log.error("Cannot start serer.", e);
+            }
+        } else {
+            log.warn("Server is already running.");
         }
     }
 
-    public void start() throws ServletException {
-        DeploymentInfo servletBuilder = Servlets.deployment() //
-                .setClassLoader(getClass().getClassLoader()) //
-                .setContextPath("/") //
-                .setDeploymentName("bank.war")//
-                .addServlets(//
-                        Servlets.servlet("BankServlet", org.glassfish.jersey.servlet.ServletContainer.class)//
-                                .addInitParam("javax.ws.rs.Application", "org.example.amt.rest.BankApplication")//
-                                .addMapping("/*"));
-
-        DeploymentManager manager = Servlets.defaultContainer().addDeployment(servletBuilder);
-        manager.deploy();
-
-        PathHandler path = Handlers.path(manager.start());
-
-        server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setIoThreads(2)
-                .setWorkerThreads(10)
-                .setHandler(path)
-                .build();
-        server.start();
-    }
-
     public void stop() {
-        if (server != null) {
+        if (server == null) {
+            log.warn("Server is not running.");
+        } else {
+            log.info("Stopping server.");
             server.stop();
         }
     }
